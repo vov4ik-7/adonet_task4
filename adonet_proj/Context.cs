cdf0d91943fa2a85c9803bb022213201ecc1f204
Volodymyr Lysyshyn <lysyshynvova@gmail.com>
2018-12-08 00:54:03 +0200

    Closed #1 - Add context.

---
+++ adonet_proj/Context.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adonet_proj
+{
+    public class Context
+    {
+        private string connectionString;
+        private SqlConnection connection;
+
+        public Context(string connectionStr)
+        {
+            connection = new SqlConnection(connectionStr);
+            connection.Open();
+        }
+        public void AllInfoID8()
+        {
+            Console.WriteLine("> Show all info about the employee with ID 8.");
+            SqlCommand command = connection.CreateCommand();
+            command.CommandText = "select * from Employees where EmployeeID=8";
+            SqlDataReader reader = command.ExecuteReader();
+            reader.Read();
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 1; i < reader.FieldCount; i++)
+            {
+                stringBuilder.AppendLine($"\t{reader.GetName(i)}: {reader.GetValue(i).ToString()}");
+            }
+            Console.WriteLine(stringBuilder.ToString());
+        }
+    }
+}
---
+++ adonet_proj/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace adonet_proj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            Context context = new Context(connectionString);
+
+            context.AllInfoID8();
+
+            Console.ReadKey();
+        }
+    }
+}
